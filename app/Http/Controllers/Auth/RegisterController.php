3f7bffa4fbc020113ae51e4503bdda5e8fae4114
Rusty <decolongonrusty23@gmail.com>
2026-07-28 17:29:55 +0800

    add session regenerate

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -42,6 +42,7 @@ class RegisterController extends Controller
         ]);
 
         Auth::login($user);
+        $request->session()->regenerate();
         return redirect()->intended(route('dashboard'));
     }
 }
