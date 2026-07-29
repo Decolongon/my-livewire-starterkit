3cd973247e4c3e528f297ec5b35034c45e87f5bf
Rusty <decolongonrusty23@gmail.com>
2026-07-29 22:47:20 +0800

    fix blaze

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->configureRateLimiting();
-        this->blazeConfig();
+        $this->blazeConfig();
     }
 
     protected function configureRateLimiting(): void
