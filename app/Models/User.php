cc158ba12e0943b62aa6f5d57d76493dc9aeb931
Rusty <decolongonrusty23@gmail.com>
2026-08-05 22:37:20 +0800

    add initials and eloquent atrributes

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,12 @@ namespace App\Models;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
@@ -28,5 +31,17 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function initial(): string
+    {
+        return Str::initials(ucwords($this->name));
+    }
+
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            set: fn(string $value) => Str::title($value),
+        );
     }
 }
